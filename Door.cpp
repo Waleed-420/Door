392383bfad77e7017a93296038d44050938ad7f3
Muhmmad Waleed <L1S21BSSE0063@ucp.edu.pk>
2024-01-28 17:41:55 +0500

    Added door class definbation in new cpp file

---
+++ Door.cpp
@@ -0,0 +1,19 @@
+#inlcude <iostream>
+using namespace std;
+
+class door
+{
+private:
+bool isOpen;
+bool isLocked;
+public:
+Door();
+void opendoor();
+void closedoor();
+void lockdoor();
+bool isDoorOpen();
+bool isDoorLocked();
+};
+int main()
+{
+}
